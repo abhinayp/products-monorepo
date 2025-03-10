5695059bb3574a388469a0a87a112ffa19b3c0f2
Abhinay Parvathaneni <pabhinay43@gmail.com>
2025-03-10 02:53:12 -0400

    add consumer to inventory and producer to cart

--- cart/app/producers/InventoryProducer.rb
+++ cart/app/producers/InventoryProducer.rb
@@ -1,6 +1,5 @@
 class InventoryProducer
-  def self.update_cart_metrics product_id
-    item_count = CartItem.where(product_id: product_id).count
+  def self.update_cart_metrics product_id:, new_item_count:
     user_count = CartItem.where(product_id: product_id).distinct.count(:user_id)
 
     Karafka.producer.produce_async(
@@ -8,7 +7,7 @@ class InventoryProducer
       payload: {
         event: 'update_cart_metrics',
         product_id: product_id,
-        item_count: item_count,
+        new_item_count: new_item_count,
         user_count: user_count
       }.to_json
     )
