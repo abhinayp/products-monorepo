72de516d93c500ede4f247066a3b5a73f248feb4
Abhinay Parvathaneni <pabhinay43@gmail.com>
2025-03-09 22:57:20 -0400

    create signup page

--- frontend/src/components/signup-form.tsx
+++ frontend/src/components/signup-form.tsx
@@ -5,19 +5,31 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import Link from "next/link"
 
-export function LoginForm({
+export function SignUpForm({
   className,
   ...props
 }: React.ComponentProps<"form">) {
   return (
     <form className={cn("flex flex-col gap-6", className)} {...props}>
       <div className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-2xl font-bold">Login to your account</h1>
+        <h1 className="text-2xl font-bold">Create an account</h1>
         <p className="text-muted-foreground text-sm text-balance">
-          Enter your email below to login to your account
+          Enter the details to start shopping
         </p>
       </div>
       <div className="grid gap-6">
+        <div className="grid gap-3">
+          <Label htmlFor="firstname">First Name</Label>
+          <Input id="firstname" type="text" placeholder="John" required />
+        </div>
+        <div className="grid gap-3">
+          <Label htmlFor="lastname">Last Name</Label>
+          <Input id="lastname" type="text" placeholder="Doe" required />
+        </div>
+        <div className="grid gap-3">
+          <Label htmlFor="phone">Phone</Label>
+          <Input id="phone" type="tel" placeholder="9999999999" required />
+        </div>
         <div className="grid gap-3">
           <Label htmlFor="email">Email</Label>
           <Input id="email" type="email" placeholder="m@example.com" required />
@@ -35,7 +47,7 @@ export function LoginForm({
           <Input id="password" type="password" required />
         </div>
         <Button type="submit" className="w-full">
-          Login
+          Sign Up
         </Button>
         <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
           <span className="bg-background text-muted-foreground relative z-10 px-2">
@@ -44,9 +56,9 @@ export function LoginForm({
         </div>
       </div>
       <div className="text-center text-sm">
-        Don&apos;t have an account?{" "}
-        <Link href="/signup" className="underline underline-offset-4">
-          Sign up
+        Already have an account?{" "}
+        <Link href="/login" className="underline underline-offset-4">
+          Login
         </Link>
       </div>
     </form>
