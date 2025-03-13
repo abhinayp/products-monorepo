c217900eabed1dd62217f4622a99fb0bb1edc6cc
Abhinay Parvathaneni <pabhinay43@gmail.com>
2025-03-13 01:44:32 -0400

    add logger for consumer events in websockets

--- websockets/src/consumers/consumer.controller.ts
+++ websockets/src/consumers/consumer.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Logger } from "@nestjs/common";
 import { Payload, Ctx, KafkaContext, EventPattern } from "@nestjs/microservices";
 import { CartDTO } from "./dto/cart.dto";
 import { CartTopic } from "./topics/cart.topic";
@@ -10,6 +10,8 @@ export class ConsumerController {
 
   @EventPattern('cart')
   async cart(@Payload() data: CartDTO, @Ctx() context: KafkaContext) {
+    const logger = new Logger("CartTopic");
+    logger.log(`Received cart event: '${data.event}', data: ${JSON.stringify(data)}`);
     switch (data.event) {
       case 'update_metrics':
         await this.cartTopic.updateMetrics(data, context);
