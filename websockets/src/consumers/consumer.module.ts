72213d99314d3f2d1c1708e942b0b7aaa7d6dc71
Abhinay Parvathaneni <pabhinay43@gmail.com>
2025-03-13 01:48:56 -0400

    trigger websocket events for update metrics

--- websockets/src/consumers/consumer.module.ts
+++ websockets/src/consumers/consumer.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConsumerController } from './consumer.controller';
 import { CartTopic } from './topics/cart.topic';
+import { GatewayModule } from 'src/gateway/gateway.module';
 
 @Module({
-  imports: [],
+  imports: [GatewayModule],
   controllers: [ConsumerController],
   providers: [CartTopic],
 })
--- websockets/src/consumers/topics/cart.topic.ts
+++ websockets/src/consumers/topics/cart.topic.ts
@@ -1,12 +1,18 @@
 import { Injectable, Logger } from "@nestjs/common";
 import { CartDTO } from "../dto/cart.dto";
 import { KafkaContext } from "@nestjs/microservices";
-
+import { ProductssGateway } from "src/gateway/products.gateway";
 @Injectable()
 export class CartTopic {
   private readonly logger = new Logger("CartTopic");
 
+  constructor(private readonly productsGateway: ProductssGateway) {}
+
   async updateMetrics(data: CartDTO, context: KafkaContext) {
     this.logger.log(data.user_count);
+    this.productsGateway.server.to(data.product_id).emit('update_metrics', {
+      user_count: data.user_count,
+      product_id: data.product_id,
+    });
   }
 }
