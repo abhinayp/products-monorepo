587337108d9732cf974e9ec9a524f174ad3cc8f9
Abhinay Parvathaneni <pabhinay43@gmail.com>
2025-03-13 01:51:55 -0400

    change decorator to subscribe topic

--- websockets/src/consumers/consumer.controller.ts
+++ websockets/src/consumers/consumer.controller.ts
@@ -1,14 +1,15 @@
 import { Controller, Logger } from "@nestjs/common";
-import { Payload, Ctx, KafkaContext, EventPattern } from "@nestjs/microservices";
+import { Payload, Ctx, KafkaContext } from "@nestjs/microservices";
 import { CartDTO } from "./dto/cart.dto";
 import { CartTopic } from "./topics/cart.topic";
+import { SubscribeTopic } from "src/decorators/topic.decorator";
 
 @Controller('')
 export class ConsumerController {
 
   constructor(private readonly cartTopic: CartTopic) {}
 
-  @EventPattern('cart')
+  @SubscribeTopic('cart')
   async cart(@Payload() data: CartDTO, @Ctx() context: KafkaContext) {
     const logger = new Logger("CartTopic");
     logger.log(`Received cart event: '${data.event}', data: ${JSON.stringify(data)}`);
