4d5e52784043ecbaca374caef89989152ece7fbf
Abhinay Parvathaneni <pabhinay43@gmail.com>
2025-03-23 12:18:21 -0400

    update product skeleton, move logic to useProduct

--- frontend/src/components/Products/Product/Product.tsx
+++ frontend/src/components/Products/Product/Product.tsx
@@ -3,27 +3,49 @@
 import { Card, CardContent, CardFooter } from "../../ui/card"
 import { Badge } from "../../ui/badge"
 import { Button } from "../../ui/button"
-import { ChevronDown, Heart, ShoppingCart, Users } from "lucide-react"
+import { Users } from "lucide-react"
 import Image from "next/image"
-import Link from "next/link"
-import useProduct from "./useProduct"
+import useProduct, { UseProductProps } from "./useProduct"
 
-interface Props {
-  id: number
-  title: string
-  price: number
-  description: string
-  category: string
-  image_url: string
-  available_count?: number
-  hold_count?: number
-  sold_count?: number
-  cart_users_count?: number
-  your_cart_count?: number
-}
+interface Props extends UseProductProps { }
 
 const Product = (product: Props) => {
-  const { addToCart, isPending } = useProduct({ productId: product.id })
+  const {
+    addToCart,
+    isPending,
+    lowStock,
+    outOfStock,
+  } = useProduct({ product: product })
+
+  // Stock availability badge
+  let stockAvailabilityBadgeView = null
+  if (lowStock) {
+    stockAvailabilityBadgeView = (
+      <p className="text-xs text-yellow-600 dark:text-yellow-500">
+        Only {product.available_count} left in stock
+      </p>
+    )
+  } else if (outOfStock) {
+    stockAvailabilityBadgeView = (
+      <p className="text-xs text-destructive">
+        Out of stock
+      </p>
+    )
+  }
+
+  // Cart users badge
+  let cartUsersBadgeView = null
+  if (product.cart_users_count) {
+    cartUsersBadgeView = (
+      <div className="absolute top-2 right-2">
+        <Badge variant="secondary" className="gap-1.5">
+          <Users className="h-3 w-3" />
+          {product.cart_users_count} {product.cart_users_count > 1 ? "others" : "other"} added to cart
+        </Badge>
+      </div>
+    )
+  }
+
 
   return (
     <Card key={product.id} className="overflow-hidden group p-0">
@@ -36,28 +58,12 @@ const Product = (product: Props) => {
             height={400}
             className="h-64 w-full object-cover transition-transform duration-300 group-hover:scale-105"
           />
-          {product.cart_users_count !== undefined && product.cart_users_count > 0 && (
-            <div className="absolute top-2 right-2">
-              <Badge variant="secondary" className="gap-1.5">
-                <Users className="h-3 w-3" />
-                {product.cart_users_count} {product.cart_users_count > 1 ? "others" : "other"} added to cart
-              </Badge>
-            </div>
-          )}
+          {cartUsersBadgeView}
         </div>
         <CardContent className="p-4">
           <div className="flex justify-between">
             <div className="text-sm text-muted-foreground mb-1 capitalize">{product.category}</div>
-            {product.available_count !== undefined && product.available_count <= 5 && product.available_count > 0 && (
-            <p className="text-xs text-yellow-600 dark:text-yellow-500">
-              Only {product.available_count} left in stock
-            </p>
-          )}
-          {product.available_count === 0 && (
-            <p className="text-xs text-destructive">
-              Out of stock
-            </p>
-          )}
+            {stockAvailabilityBadgeView}
           </div>
           <h3 className="font-medium text-lg truncate">{product.title}</h3>
           <p className="text-sm text-muted-foreground line-clamp-2 h-10">{product.description}</p>
@@ -77,20 +83,22 @@ const Product = (product: Props) => {
   )
 }
 
+// Loading skeleton
 const ProductSkeleton = () => {
   return (
-    <Card className="group overflow-hidden">
-      <CardContent className="pt-4">
-        <div className="h-48 w-full bg-gray-200 animate-pulse rounded-lg"></div>
-        <div className="mt-4">
+    <Card className="group overflow-hidden p-0">
+        <div className="h-63 w-full bg-gray-200 animate-pulse rounded-lg"></div>
+      <CardContent className="p-4 pt-0">
           <div className="h-4 w-32 bg-gray-200 animate-pulse rounded"></div>
           <div className="h-4 w-24 bg-gray-200 animate-pulse rounded mt-2"></div>
           <div className="h-4 w-24 bg-gray-200 animate-pulse rounded mt-2"></div>
-        </div>
       </CardContent>
-      <CardFooter className="flex justify-between items-center pt-0">
+      <CardFooter className="flex justify-between items-center p-4 pt-0">
         <div>
           <div className="h-4 w-24 bg-gray-200 animate-pulse rounded"></div>
+        </div>
+        <div>
+          <div className="h-8 w-25 bg-zinc-600 animate-pulse rounded-lg"></div>
         </div>
       </CardFooter>
     </Card>
--- frontend/src/components/Products/Product/useProduct.ts
+++ frontend/src/components/Products/Product/useProduct.ts
@@ -4,11 +4,25 @@ import { useQueryClient } from "@tanstack/react-query"
 import { productsClient } from "@/api-client/inventory/products.client"
 import { toast } from "sonner"
 
-const useProduct = ({ productId }: { productId: number }) => {
+export interface UseProductProps {
+  id: number
+  title: string
+  price: number
+  description: string
+  category: string
+  image_url: string
+  available_count?: number
+  hold_count?: number
+  sold_count?: number
+  cart_users_count?: number
+  your_cart_count?: number
+}
+
+const useProduct = ({ product }: { product: UseProductProps }) => {
   const { setShowCart } = useGlobal()
   const queryClient = useQueryClient()
   const { mutate: addToCart, isPending } = useMutation({
-    mutationFn: () => productsClient.addToCart({ id: productId }),
+    mutationFn: () => productsClient.addToCart({ id: product.id }),
     onSuccess: () => {
       setShowCart(true)
       queryClient.invalidateQueries({ queryKey: ['products'] })
@@ -19,10 +33,14 @@ const useProduct = ({ productId }: { productId: number }) => {
       })
     },
   })
+  const lowStock = product.available_count !== undefined && product.available_count <= 5 && product.available_count > 0
+  const outOfStock = product.available_count === 0
 
   return {
     addToCart,
     isPending,
+    lowStock,
+    outOfStock,
   }
 }
 
